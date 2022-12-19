5565d134086e70a4279bf5f7a40a8663e5ca0c43
Daniel <danielspradah0@gmail.com>
2022-12-19 03:12:26 -0500

    Fix body event behavior

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,18 @@
 const book = document.querySelectorAll(".book-card");
 const popup = document.getElementById("popup");
-const submitBtn = document.getElementById("submitBtn");
 const addBtn = document.getElementById("addBtn");
 const deleteBtn = document.getElementById("deleteBtn");
 const body = document.getElementById("body");
 const readUi = document.querySelectorAll(".readUI");
 const newCard = document.createElement("div");
 const lib = document.querySelector(".book-section");
+
+const form = document.querySelector(".form");
+const bookTitle = document.getElementById("bookTitle");
+const authorName = document.getElementById("authorName");
+const readCheck = document.getElementById("readCheck");
+const submitBtn = document.getElementById("submitBtn");
+const inputField = document.querySelectorAll(".input");
 
 let bookCounter = book.length -1;
 
@@ -16,9 +22,14 @@ function Book(title, author) {
 }
 
 addBtn.addEventListener("click", (e) => {
-    popup.style.cssText ="transform: translate(-50%, -50%) scale(1); visibility: visible; transition: 0.2s ease-in;"
-    body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-    e.stopPropagation();
+    if (popup.style.visibility === "visible") {
+        body.click();
+    } else {
+        popup.style.cssText ="transform: translate(-50%, -50%) scale(1); visibility: visible; transition: 0.2s ease-in;"
+        body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        e.stopPropagation();
+    }
+
 });
 
 submitBtn.addEventListener("click", () => {
@@ -27,12 +38,27 @@ submitBtn.addEventListener("click", () => {
 })
 
 deleteBtn.addEventListener("click", () => {
-    for (let i = 0; i < book.length; i++) {
-        if (book[i].style.border === "2px solid black") {
-            book[i].style.border = "none";
-            lib.removeChild(book[i]);
+    if (popup.style.visibility === "visible") {
+        body.click();
+    } else {
+        for (let i = 0; i < book.length; i++) {
+            if (book[i].style.border === "2px solid black") {
+                book[i].style.border = "none";
+                lib.removeChild(book[i]);
+            }
         }
     }
+
+})
+
+for (let i = 0; i < inputField.length; i++){
+    inputField[i].addEventListener("click", (e) => {
+        e.stopPropagation();
+    })
+}
+
+popup.addEventListener("click", (e) => {
+    e.stopPropagation();
 })
 
 function setReadId() {
@@ -42,11 +68,16 @@ function setReadId() {
         let currentRead = readUi[i];
         currentRead.addEventListener("click", (e) => {
             e.stopPropagation();
-            if (currentRead.style.backgroundColor === "rgb(159, 255, 156)") {
-                currentRead.style.backgroundColor = "#e58c8c"
+            if (popup.style.visibility === "visible") {
+                body.click();
             } else {
-                currentRead.style.cssText = "background-color: #9fff9c";
+                if (currentRead.style.backgroundColor === "rgb(159, 255, 156)") {
+                    currentRead.style.backgroundColor = "#e58c8c"
+                } else {
+                    currentRead.style.cssText = "background-color: #9fff9c";
+                }
             }
+
     
         })
     }
@@ -56,30 +87,42 @@ function setBooksId() {
         book[i].id = `book-${i}`;
         let currentBook = book[i];
         currentBook.addEventListener("click", () => {
-            if (currentBook.style.border === "2px solid black"){
-                currentBook.style.cssText = "border: none;";
-            }else {
-                currentBook.style.cssText = "border: 2px solid black;";
+            if (popup.style.visibility === "visible") {
+                body.click();
+            } else {
+                if (currentBook.style.border === "2px solid black"){
+                    currentBook.style.cssText = "border: none;";
+                }else {
+                    currentBook.style.cssText = "border: 2px solid black;";
+                }
             }
+
     
         })
     }
 }
 
-setBooksId();
-setReadId();
+
 
 
 body.addEventListener("click", () => {
     if (popup.style.visibility == "visible") {
-        popup.style.cssText = "transform: translate(-50%, -50%) scale(0.1);";
+        popup.style.cssText = "transform: translate(-50%, -50%) scale(0.1); visibility: hidden;";
         body.style.backgroundColor = "rgba(0, 0, 0, 0)";
     }
 })
 
+form.addEventListener("submit", function(event) {
+    // Prevent the form from being submitted
+    event.preventDefault();
+
+  
+    // Log the data object to the console
+    console.log(data);
+  });
 
 
-
-
+setBooksId();
+setReadId();
 // newCard.className = "book-card";
 // lib.appendChild(newCard);
