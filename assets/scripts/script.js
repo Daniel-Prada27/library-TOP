81c6eabbceddbe2a6f18afee80175c9344e8a449
Daniel <danielspradah0@gmail.com>
2022-12-19 00:56:09 -0500

    Add visual features

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-const book = document.querySelector(".book-card");
+const book = document.querySelectorAll(".book-card");
 const popup = document.getElementById("popup");
 const submitBtn = document.getElementById("submitBtn");
 const addBtn = document.getElementById("addBtn");
@@ -13,3 +13,18 @@ submitBtn.addEventListener("click", () => {
     popup.style.cssText ="transform: translate(-50%, -50%) scale(0.1); visibility: hidden; transition: 0.2s ease-out;"
     body.style.backgroundColor = "rgba(0, 0, 0, 0)";
 })
+
+
+
+for (let i = 0; i < book.length; i++) {
+    book[i].id = `book-${i}`;
+    let currentBook = book[i];
+    book[i].addEventListener("click", () => {
+        if (currentBook.style.border === "2px solid black"){
+            currentBook.style.cssText = "border: none;";
+        }else {
+            currentBook.style.cssText = "border: 2px solid black;";
+        }
+
+    })
+}
